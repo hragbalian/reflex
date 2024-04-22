e7c634bf37fffce780cc49a7f7d7458dcf3917f0
Hrag Balian <hbalian@lrwonline.com>
2024-04-22 12:19:27 -0700

    feat: added mermaid remark plugin

--- reflex/components/markdown/markdown.py
+++ reflex/components/markdown/markdown.py
@@ -32,7 +32,8 @@ _MOCK_ARG = Var.create_safe("")
 _REMARK_MATH = Var.create_safe("remarkMath", _var_is_local=False)
 _REMARK_GFM = Var.create_safe("remarkGfm", _var_is_local=False)
 _REMARK_UNWRAP_IMAGES = Var.create_safe("remarkUnwrapImages", _var_is_local=False)
-_REMARK_PLUGINS = Var.create_safe([_REMARK_MATH, _REMARK_GFM, _REMARK_UNWRAP_IMAGES])
+_REMARK_MERMAID = Var.create_safe("remarkMermaid", _var_is_local=False)
+_REMARK_PLUGINS = Var.create_safe([_REMARK_MATH, _REMARK_GFM, _REMARK_UNWRAP_IMAGES, _REMARK_MERMAID])
 
 # Special rehype plugins.
 _REHYPE_KATEX = Var.create_safe("rehypeKatex", _var_is_local=False)
