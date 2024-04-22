f8503826b32c7f32f2b931994b76b5bc559cbdbf
Hrag Balian <hbalian@lrwonline.com>
2024-04-22 13:04:43 -0700

    feat: trying to change the plugin version

--- reflex/components/markdown/markdown.py
+++ reflex/components/markdown/markdown.py
@@ -175,7 +175,7 @@ class Markdown(Component):
                 "remark-unwrap-images@4.0.0": [
                     ImportVar(tag=_REMARK_UNWRAP_IMAGES._var_name, is_default=True)
                 ],
-                "remark-mermaidjs@6.0.0": [
+                "remark-mermaid-plugin@1.0.2": [
                     ImportVar(tag=_REMARK_MERMAID._var_name, is_default=True)
                 ],
                 "rehype-katex@6.0.3": [
